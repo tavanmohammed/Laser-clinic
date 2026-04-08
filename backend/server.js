0ba8023402725cea9d8f2d7778775158292731d7
Tavan Mohammed <tavan.mohammed19@gmail.com>
2026-04-08 18:39:17 -0400

    Remove hardcoded CORS origin URL

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,6 @@ const app = express();
 app.use(
   cors({
     origin: process.env.CLIENT_URL || "http://localhost:5173",
-    "https://laser-clinic.onrender.com"
     credentials: true,
   })
 );
