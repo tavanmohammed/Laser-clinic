de01b2e6033fb483900680d22fd01f32e8e19a49
Tavan Mohammed <tavan.mohammed19@gmail.com>
2026-04-08 14:10:46 -0400

    Rename bookingController import to match file case

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const bookingController = require("../controllers/bookingController");
+const bookingController = require("../controllers/BookingController");
 
 router.get("/availability", bookingController.getAvailability);
 router.get("/", bookingController.getAllBookings);
