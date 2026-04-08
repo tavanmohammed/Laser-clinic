2d31e83a6113bd6fec0bbf8738f09ef448b1d9f9
Tavan Mohammed <tavan.mohammed19@gmail.com>
2026-04-08 18:24:23 -0400

    Add additional CORS origin for production

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ const app = express();
 app.use(
   cors({
     origin: process.env.CLIENT_URL || "http://localhost:5173",
+    "https://laser-clinic.onrender.com"
     credentials: true,
   })
 );
