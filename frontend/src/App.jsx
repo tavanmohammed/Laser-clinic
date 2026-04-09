bb6c4e391cfbc5ebfdcdd74b347f288f8323ff0f
Tavan Mohammed <tavan.mohammed19@gmail.com>
2026-04-09 00:53:33 -0400

    Remove Footer component from App
    Removed Footer component from MainLayout.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,7 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/gallery" element={<Gallery />} />
       </Routes>
-      <Footer />
+      
     </MainLayout>
   );
 }
