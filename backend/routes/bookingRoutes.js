23da1495eac81d2e275d7325210457cf90c2e567
Tavan Mohammed <tavan.mohammed19@gmail.com>
2026-04-08 22:12:42 -0400

    Replace availability route with a test response

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -2,7 +2,14 @@ const express = require("express");
 const router = express.Router();
 const bookingController = require("../controllers/BookingController");
 
-router.get("/availability", bookingController.getAvailability);
+router.get("/availability", (req, res) => {
+  res.json({
+    ok: true,
+    message: "availability route works",
+    query: req.query,
+  });
+});
+
 router.get("/", bookingController.getAllBookings);
 router.post("/", bookingController.createBooking);
 router.put("/:id", bookingController.updateBooking);
