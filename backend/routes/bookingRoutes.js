9e80c292950a4afb05db91adc26de3fd3aaaaac2
Tavan Mohammed <tavan.mohammed19@gmail.com>
2026-04-08 22:38:54 -0400

    Refactor availability route to use controller

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -2,13 +2,8 @@ const express = require("express");
 const router = express.Router();
 const bookingController = require("../controllers/BookingController");
 
-router.get("/availability", (req, res) => {
-  res.json({
-    ok: true,
-    route: "availability works",
-    query: req.query,
-  });
-});
+router.get("/availability", bookingController.getAvailability);
+
 router.get("/", bookingController.getAllBookings);
 router.post("/", bookingController.createBooking);
 router.put("/:id", bookingController.updateBooking);
